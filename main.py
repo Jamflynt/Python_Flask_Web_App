8fa97728804bece5a37b4cf291c30364ee4edede
Jamie Flynt <jamflynt@gmail.com>
2022-07-06 11:30:11 -0400

    Changed default image file extension

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     # lazy argument as true will load the data as necessary in one go
     # Helps us get all the stuff from a user without any extra code
